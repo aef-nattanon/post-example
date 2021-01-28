3311c520728cc619527c77d6c1d92c090e223ed8
nattanon <nattanon@hospital-os.com>
2021-01-28 15:00:41 +0700

    Add logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless User.find_by(email: 'admin@post.com').present?
+  user = User.create(email: 'admin@post.com', password: 'post123')
+  user.add_role :admin
+end
+
+unless User.find_by(email: 'user@post.com').present?
+  user = User.create(email: 'user@post.com', password: 'post123')
+  user.add_role :user
+end
