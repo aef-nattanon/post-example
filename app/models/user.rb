854213e68f6c956d5c905ae25e3445fc060706e0
aef-nattanon <48983118+aef-nattanon@users.noreply.github.com>
2021-10-19 15:09:25 +0700

    Run rails generate devise_two_factor User OTP_SECRET_KEY

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  devise :two_factor_authenticatable,
+         :otp_secret_encryption_key => ENV['OTP_SECRET_KEY']
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :posts
