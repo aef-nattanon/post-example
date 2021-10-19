6180b49d00da20f5885c861bdb2a45696cf39df7
aef-nattanon <48983118+aef-nattanon@users.noreply.github.com>
2021-10-19 15:12:01 +0700

    add config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  devise :two_factor_authenticatable,
-         :otp_secret_encryption_key => ENV['OTP_SECRET_KEY']
+  devise :two_factor_authenticatable, :two_factor_backupable,
+         otp_backup_code_length: 10, otp_number_of_backup_codes: 10,
+         otp_secret_encryption_key: ENV['OTP_SECRET_KEY']
 
   rolify
   # Include default devise modules. Others available are:
